b71b6f651c74e078005eb84e9e5fb5387aa78fb3
Ani <anirudho747@gmail.com>
2024-08-22 20:42:37 +0530

    first commit

---
+++ src/test/java/EcommerceAPITest.java
@@ -0,0 +1,36 @@
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import pojo.LoginRequest;
+import pojo.LoginResponse;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+
+public class EcommerceAPITest {
+
+    public static void main (String[] args)
+    {
+        RequestSpecification req =  new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
+                                         .setContentType(ContentType.JSON)
+                                         .build();
+        LoginRequest lR = new LoginRequest();
+
+        lR.setUserEmail("Learner@gmail.com");
+        lR.setUserPassword("Learner@1234");
+
+        RequestSpecification reqLogin = given().spec(req).body(lR);
+//When we use String Extraction
+                String responseString = reqLogin.when().post("api/ecom/auth/login")
+                                        .then().extract().response().asString();
+
+//When we use deserialization
+          LoginResponse loginResponse = reqLogin.when().post("api/ecom/auth/login")
+                                        .then().extract().response().as(LoginResponse.class);
+
+        System.out.println(loginResponse.getToken());
+        System.out.println(loginResponse.getUserId());
+
+    }
+}
